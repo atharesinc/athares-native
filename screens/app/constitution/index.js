65d0fca026fa8eda66d1de640b6dd159b3f014c4
actuallyDan <dankral01@gmail.com>
2019-04-25 17:30:47 -0400

    feat(graphql): connected Constitution to apollo-client

--- screens/app/constitution/index.js
+++ screens/app/constitution/index.js
@@ -4,40 +4,119 @@ import Amendment from "../../../components/Amendment";
 
 import { Text, ScrollView, StyleSheet } from "react-native";
 
-export default class Constitution extends Component {
-  state = {
-    edit: false
-  };
+import { connect } from "react-redux";
+import { pull } from "../../../redux/state/reducers";
+import {
+  updateCircle,
+  updateChannel,
+  updateRevision
+} from "../../../redux/state/actions";
+import { GET_AMENDMENTS_FROM_CIRCLE_ID } from "../../../graphql/queries";
+import { Query } from "react-apollo";
+import { SUB_TO_CIRCLES_AMENDMENTS } from "../../../graphql/subscriptions";
+import { UIActivityIndicator } from "react-native-indicators";
 
+class Constitution extends Component {
   toggleEdit = () => {
     this.setState({
       edit: !this.state.edit
     });
   };
 
+  componentDidMount() {
+    // we only need to manually fetch the circle and it's amendments if the user isn't signed in
+    // we should always make sure that the currently navigated-to circle is the activeCircle in redux
+    this._isMounted = true;
+    if (this.props.activeCircle) {
+      this.props.dispatch(updateChannel(null));
+      this.props.dispatch(updateRevision(null));
+    } else {
+      let circleID = this.props.match.params.id;
+
+      this.props.dispatch(updateCircle(circleID));
+      this.props.dispatch(updateChannel(null));
+      this.props.dispatch(updateRevision(null));
+    }
+  }
+  _subToMore = subscribeToMore => {
+    subscribeToMore({
+      document: SUB_TO_CIRCLES_AMENDMENTS,
+      variables: { id: this.props.activeCircle || "" },
+      updateQuery: (prev, { subscriptionData }) => {
+        let {
+          previousValues,
+          mutation,
+          node: amendment
+        } = subscriptionData.data.Amendment;
+        switch (mutation) {
+          case "CREATED":
+            let ind = prev.Circle.amendments.findIndex(
+              a => a.id === amendment.id
+            );
+            // if the new node isn't in the data set
+            if (ind === -1) {
+              prev.Circle.amendments = [...prev.Circle.amendments, amendment];
+            }
+            break;
+          case "UPDATED":
+            let index = prev.Circle.amendments.findIndex(
+              a => a.id === amendment.id
+            );
+            prev.Circle.amendments[index] = amendment;
+            break;
+          case "DELETED":
+            let i = prev.Circle.amendments.findIndex(
+              a => a.id === previousValues.id
+            );
+            prev.Circle.amendments.splice(i, 1);
+            break;
+          default:
+            break;
+        }
+        return prev;
+      }
+    });
+  };
   render() {
-    const { edit } = this.state;
+    let { user, activeCircle } = this.props;
+    let circle = null;
+    let amendments = [];
 
     return (
-      <ScreenWrapper styles={[styles.wrapper]}>
-        <Text style={styles.preamble}>
-          Athares is dedicated to providing democratic functionality to groups
-          and organizations everywhere.
-        </Text>
-        <ScrollView styles={[styles.wrapper]}>
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-          <Amendment />
-        </ScrollView>
-      </ScreenWrapper>
+      <Query
+        query={GET_AMENDMENTS_FROM_CIRCLE_ID}
+        variables={{ id: activeCircle || "" }}
+        fetchPolicy={"cache-and-network"}
+      >
+        {({ data, subscribeToMore }) => {
+          if (data.Circle) {
+            circle = data.Circle;
+            amendments = data.Circle.amendments;
+            this._subToMore(subscribeToMore);
+          }
+          if (circle) {
+            return (
+              <ScreenWrapper styles={[styles.wrapper]}>
+                <Text style={styles.preamble}>{circle.preamble}</Text>
+                <ScrollView styles={[styles.wrapper]}>
+                  {amendments.map((amendment, i) => (
+                    <Amendment key={i} user={user} />
+                  ))}
+                </ScrollView>
+              </ScreenWrapper>
+            );
+          } else {
+            return (
+              <ScreenWrapper
+                styles={{ justifyContent: "center", alignItems: "center" }}
+              >
+                <UIActivityIndicator color={"#FFFFFF"} style={{ flex: 0 }} />
+                <Text style={styles.loadingText}>Loading Constitution</Text>
+              </ScreenWrapper>
+            );
+          }
+        }}
+      </Query>
     );
   }
 }
@@ -54,5 +133,21 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontSize: 15,
     marginBottom: 20
+  },
+  loadingText: {
+    fontSize: 20,
+    color: "#FFFFFF",
+    marginTop: 15,
+    width: "100%",
+    textAlign: "center"
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    user: pull(state, "user"),
+    activeCircle: pull(state, "activeCircle")
+  };
+}
+
+export default connect(mapStateToProps)(Constitution);

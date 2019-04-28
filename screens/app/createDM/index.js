56f0e447c19add62c404d7fec95489e1196431e0
actuallyDan <dankral01@gmail.com>
2019-04-27 23:38:39 -0400

    feat(graphql): connected CreateDM component to apollo-client

--- screens/app/createDM/index.js
+++ screens/app/createDM/index.js
@@ -4,11 +4,32 @@ import { View, ScrollView, Text, StyleSheet } from "react-native";
 import InviteUser from "../../../components/InviteUser";
 import Chat from "../../../components/Chat";
 
-export default class CreateDM extends Component {
+import { pull } from "../../../redux/state/reducers";
+import { connect } from "react-redux";
+import { encrypt } from "../../../utils/crypto";
+import SimpleCrypto from "simple-crypto-js";
+import { GET_USER_BY_ID } from "../../../graphql/queries";
+import {
+  CREATE_CHANNEL,
+  CREATE_KEY,
+  CREATE_MESSAGE,
+  ADD_USER_TO_CHANNEL
+} from "../../../graphql/mutations";
+import { graphql, compose } from "react-apollo";
+import uploadToIPFS from "../../../utils/ipfs";
+import { UIActivityIndicator } from "react-native-indicators";
+
+class CreateDM extends Component {
   state = {
     isFocused: false,
-    tags: []
+    tags: [],
+    cryptoEnabled: false,
+    uploadInProgress: false
   };
+  updateProgress = (prog, length) => {
+    console.log(prog / length);
+  };
+
   onFocusChange = isFocused => {
     this.setState({
       isFocused
@@ -26,9 +47,126 @@ export default class CreateDM extends Component {
       </View>
     ));
   };
-  render() {
-    const { tags } = this.state;
+  submit = async (messages = []) => {
+    messages.forEach(async ({ text = "", file = null }) => {
+      let { tags } = this.state;
+      let { data } = this.props;
+      if (!data.User) {
+        return false;
+      }
+      // We're going to allow users to have no recipients because they always get added to a channel on creation
+      // This defaults to a "just you" channel but they can later add users if they like
+      // if (selectedUsers.length === 0) {
+      //   return false;
+      // }
+      // if the user addresses themselves, remove them because they'll get added anyway
+      let userIndex = tags.findIndex(u => u.id === this.props.user);
+      if (userIndex !== -1) {
+        tags.splice(userIndex, 1);
+      }
+      if (text.trim().length === 0 && file === null) {
+        return false;
+      }
+      await this.setState({
+        uploadInProgress: true
+      });
+      let { User: user } = this.props.data;
 
+      // create a symmetric key for the new channel
+      var _secretKey = SimpleCrypto.generateRandom({ length: 256 });
+
+      var simpleCrypto = new SimpleCrypto(_secretKey);
+
+      // add this user to the list of selectedUsers
+      tags.push(user);
+
+      const tempName = tags.map(u => u.firstName + " " + u.lastName).join(", ");
+
+      const newChannel = {
+        name: tempName,
+        channelType: "dm",
+        description: tempName
+      };
+
+      try {
+        // create the channel as a DM channel
+        let res = await this.props.createChannel({
+          variables: {
+            ...newChannel
+          }
+        });
+
+        let { id } = res.data.createChannel;
+
+        // give each user an encrypted copy of this keypair and store it in
+        let promiseList = tags.map(async u => {
+          const encryptedKey = await encrypt(_secretKey, u.pub);
+          return this.props.createKey({
+            variables: {
+              key: encryptedKey,
+              user: u.id,
+              channel: id
+            }
+          });
+        });
+
+        // add each user to this channel
+        let promiseList2 = tags.map(u =>
+          this.props.addUserToChannel({
+            variables: {
+              channel: id,
+              user: u.id
+            }
+          })
+        );
+        // store all the keys, add all the users
+        await Promise.all(promiseList);
+        await Promise.all(promiseList2);
+
+        let url =
+          file === null ? null : await uploadToIPFS(file, this.updateProgress);
+        if (file) {
+          fetch(file);
+        }
+        // send the first message, encrypted with the channel's SEA pair
+        let newMessage = {
+          text: simpleCrypto.encrypt(text.trim()),
+          user: this.props.user,
+          channel: id,
+          file: url ? simpleCrypto.encrypt(url) : "",
+          fileName: file !== null ? file.name : null
+        };
+
+        this.props.createMessage({
+          variables: {
+            ...newMessage
+          }
+        });
+        await this.setState({
+          uploadInProgress: false
+        });
+        this.props.navigation.navigate(`DMChannel`);
+      } catch (err) {
+        console.error(new Error(err));
+        Alert.alert(
+          "Error",
+          "We were unable to send your message, please try again later"
+        );
+      }
+    });
+  };
+
+  render() {
+    const { tags, loading } = this.state;
+    if (loading || uploadInProgress) {
+      return (
+        <ScreenWrapper
+          styles={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <UIActivityIndicator color={"#FFFFFF"} />
+        </ScreenWrapper>
+      );
+    }
     return (
       <ScreenWrapper styles={[styles.wrapper]}>
         <View
@@ -55,24 +193,16 @@ export default class CreateDM extends Component {
             </View>
           )}
           <InviteUser
-            suggestions={suggestions}
             tags={tags}
             updateTags={this.updateTags}
             onFocusChange={this.onFocusChange}
           />
         </View>
-        <Chat />
+        <Chat user={user} messages={[]} sendMessages={this.submit} />
       </ScreenWrapper>
     );
   }
 }
-//           {Platform.OS === "android" ? <KeyboardSpacer /> : null}
-var suggestions = [
-  { name: "Jim", id: "123123" },
-  { name: "Dan", id: "!2315" },
-  { name: "Brian", id: "9182763" },
-  { name: "Kyle", id: "48739201" }
-];
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -111,3 +241,21 @@ const styles = StyleSheet.create({
     fontSize: 15
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    user: pull(state, "user")
+  };
+}
+
+export default connect(mapStateToProps)(
+  compose(
+    graphql(CREATE_MESSAGE, { name: "createMessage" }),
+    graphql(ADD_USER_TO_CHANNEL, { name: "addUserToChannel" }),
+    graphql(CREATE_CHANNEL, { name: "createChannel" }),
+    graphql(CREATE_KEY, { name: "createKey" }),
+    graphql(GET_USER_BY_ID, {
+      options: ({ user }) => ({ variables: { id: user || "" } })
+    })
+  )(withRouter(CreateDM))
+);

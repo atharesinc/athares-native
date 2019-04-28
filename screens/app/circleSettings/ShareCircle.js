8748262caa1dc4ca507a12af9166bdadad1d4994
actuallyDan <dankral01@gmail.com>
2019-04-27 23:19:57 -0400

    feat(graphql): connected CircleSettings-shareCircle component to apollo-client

---
+++ screens/app/circleSettings/ShareCircle.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import LinkText from "../../../components/LinkText";
+
+import { Text, View, StyleSheet, Alert } from "react-native";
+
+import { CREATE_INVITE } from "../../../graphql/mutations";
+import { GET_CIRCLE_NAME_BY_ID } from "../../../graphql/queries";
+import { graphql, Query } from "react-apollo";
+
+import { connect } from "react-redux";
+import { pull } from "../../../redux/state/reducers";
+
+class ShareCircle extends Component {
+  state = {
+    loading: false,
+    showLink: false,
+    link: ""
+  };
+  generateLink = async () => {
+    let { user, activeCircle } = this.props;
+    await this.setState({
+      loading: true,
+      link: null
+    });
+
+    try {
+      let link = await this.props.createInvite({
+        variables: {
+          inviter: user,
+          circle: activeCircle
+        }
+      });
+
+      let { id } = link.data.createInvite;
+
+      this.setState({
+        link: "https://www.athares.us/invite/" + id,
+        loading: false
+      });
+    } catch (err) {
+      console.error(new Error(err));
+      this.setState({
+        loading: false
+      });
+      Alert.alert("Error", "Unable to generate invite link.");
+    }
+  };
+  render() {
+    const { showLink, link } = this.state;
+    return (
+      <Query
+        query={GET_CIRCLE_NAME_BY_ID}
+        variables={{ id: this.props.activeCircle }}
+      >
+        {({ loading, data: { Circle: circle } }) => {
+          if (loading) {
+            return (
+              <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <UIActivityIndicator color="#FFFFFF" />
+              </View>
+            );
+          }
+          return (
+            <View style={styles.section}>
+              <Text style={styles.sectionHeading}>Share Circle</Text>
+              <Text style={styles.disclaimer}>
+                Invite someone to Athares Inc with a single-use link.
+                Prospective users will have the option to sign up if they don't
+                have an Athares account.
+              </Text>
+              {this.state.loading ? (
+                <UIActivityIndicator color="#FFFFFF" />
+              ) : (
+                <PortalButton
+                  title={"Generate Link"}
+                  onPress={this.generateLink}
+                  style={{ marginBottom: 15 }}
+                />
+              )}
+              {showLink && <LinkText text={link} />}
+            </View>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  section: {
+    marginTop: 20,
+    marginHorizontal: 15,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderColor: "#FFFFFF",
+    paddingBottom: 20
+  },
+  sectionHeading: {
+    fontSize: 20,
+    color: "#FFFFFF",
+    marginBottom: 10
+  },
+  disclaimer: {
+    fontSize: 15,
+    color: "#FFFFFFb7",
+    marginBottom: 20
+  }
+});
+function mapStateToProps(state) {
+  return {
+    user: pull(state, "user"),
+    activeCircle: pull(state, "activeCircle")
+  };
+}
+export default graphql(CREATE_INVITE, {
+  name: "createInvite"
+})(connect(mapStateToProps)(withRouter(ShareCircle)));
--- screens/app/circleSettings/index.js
+++ screens/app/circleSettings/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 import ScreenWrapper from "../../../components/ScreenWrapper";
 import PortalButton from "../../../components/PortalButton";
-import LinkText from "../../../components/LinkText";
 import CirclePrefs from "./CirclePrefs";
 
 import {
@@ -10,94 +9,53 @@ import {
   View,
   ScrollView,
   StyleSheet,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Alert
 } from "react-native";
-import { UIActivityIndicator } from "react-native-indicators";
+
+import { updateCircle } from "../../../redux/state/actions";
 
 import { connect } from "react-redux";
 import { pull } from "../../../redux/state/reducers";
 
 class CircleSettings extends Component {
-  state = {
-    showLink: false,
-    link: "",
-    loading: false,
-    email: false,
-    newRevisions: false,
-    newAmendments: false
-  };
-  generateLink = async () => {
-    await this.setState({
-      loading: true
-    });
-    this.setState({
-      showLink: true,
-      link:
-        "https://athares.us/invite/" +
-        (Math.random() * 100000000 + "").toString(16).replace(".", "")
-    });
-    await this.setState({
-      loading: false
-    });
-  };
-
-  leaveCircle = e => {
-    e.preventDefault();
+  leaveCircle = () => {
     let { activeCircle, user } = this.props;
-
-    swal("Are you sure you'd like to leave this Circle?", {
-      buttons: {
-        cancel: "Not yet",
-        confirm: true
-      }
-    }).then(async value => {
-      if (value === true) {
-        this.props.deleteUserFomCircle({
-          variables: {
-            user,
-            circle: activeCircle
-          }
-        });
-        swal(
-          "Removed From Circle",
-          `You have left this Circle. You will have to be re-invited to participate at a later time.`,
-          "warning"
-        );
-        this.props.dispatch(updateCircle(null));
-        this.props.history.push(`/app`);
+    this.props.deleteUserFomCircle({
+      variables: {
+        user,
+        circle: activeCircle
       }
     });
+    this.props.dispatch(updateCircle(null));
+
+    this.props.navigation.navigate(`Dashboard`);
+    Alert.alert(
+      "Removed From Circle",
+      `You have left this Circle. You will have to be re-invited to participate at a later time.`,
+      "warning"
+    );
+  };
+  // Double-check the user wants to leave
+  leaveCirclePrompt = e => {
+    Alert.alert(
+      "Leave Circle",
+      "Are you sure you'd like to leave this Circle?",
+      [
+        { text: "Yes", onPress: this.leaveCircle },
+        {
+          text: "Not Yet",
+          style: "cancel"
+        }
+      ]
+    );
   };
   render() {
-    const {
-      loading,
-      showLink,
-      email,
-      newRevisions,
-      newAmendments
-    } = this.state;
     return (
       <ScreenWrapper styles={[styles.wrapper]}>
         <KeyboardAvoidingView behavior="position">
           <ScrollView styles={[styles.wrapper]}>
-            <View style={styles.section}>
-              <Text style={styles.sectionHeading}>Share Circle</Text>
-              <Text style={styles.disclaimer}>
-                Invite someone to Athares Inc with a single-use link.
-                Prospective users will have the option to sign up if they don't
-                have an Athares account.
-              </Text>
-              {loading ? (
-                <UIActivityIndicator color="#FFFFFF" />
-              ) : (
-                <PortalButton
-                  title={"Generate Link"}
-                  onPress={this.generateLink}
-                  style={{ marginBottom: 15 }}
-                />
-              )}
-              {showLink && <LinkText text={this.state.link} />}
-            </View>
+            <ShareCircle activeCircle={activeCircle} />
             <CirclePrefs user={user} activeCircle={activeCircle} />
             <View style={styles.section}>
               <Text style={styles.sectionHeading}>Leave Circle</Text>

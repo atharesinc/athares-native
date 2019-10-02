da0ed572ce1b51ebbb3d40e82b33d73ce3899ab6
Daniel Kral <dkral@mobiquityinc.com>
2019-10-01 23:51:52 -0400

    fix(*): upgrade packages and prettier linted

--- screens/app/createChannel/index.js
+++ screens/app/createChannel/index.js
@@ -1,47 +1,47 @@
-import React, { Component } from "react";
-import ScreenWrapper from "../../../components/ScreenWrapper";
-import Input from "../../../components/Input";
+import React, { Component } from 'react';
+import ScreenWrapper from '../../../components/ScreenWrapper';
+import Input from '../../../components/Input';
 
 import {
   Text,
   ScrollView,
   StyleSheet,
   KeyboardAvoidingView,
-  Alert
-} from "react-native";
-import PortalButton from "../../../components/PortalButton";
-import { UIActivityIndicator } from "react-native-indicators";
+  Alert,
+} from 'react-native';
+import PortalButton from '../../../components/PortalButton';
+import { UIActivityIndicator } from 'react-native-indicators';
 import {
   CREATE_CHANNEL,
-  ADD_CHANNEL_TO_CIRCLE
-} from "../../../graphql/mutations";
-import { GET_CIRCLE_NAME_BY_ID } from "../../../graphql/queries";
-import { compose, graphql, Query } from "react-apollo";
-import { updateChannel } from "../../../redux/state/actions";
+  ADD_CHANNEL_TO_CIRCLE,
+} from '../../../graphql/mutations';
+import { GET_CIRCLE_NAME_BY_ID } from '../../../graphql/queries';
+import { compose, graphql, Query } from 'react-apollo';
+import { updateChannel } from '../../../redux/state/actions';
 
-import { connect } from "react-redux";
-import { pull } from "../../../redux/state/reducers";
+import { connect } from 'react-redux';
+import { pull } from '../../../redux/state/reducers';
 
 class CreateChannel extends Component {
   state = {
-    name: "",
-    description: "",
-    loading: false
+    name: '',
+    description: '',
+    loading: false,
   };
 
   updateName = text => {
     this.setState({
-      name: text
+      name: text,
     });
   };
   updateDesc = text => {
     this.setState({
-      description: text
+      description: text,
     });
   };
   submit = async e => {
     await this.setState({
-      loading: true
+      loading: true,
     });
 
     if (this.state.name.trim().length === 0) {
@@ -56,14 +56,14 @@ class CreateChannel extends Component {
     let newChannel = {
       name,
       description,
-      channelType: "group"
+      channelType: 'group',
     };
 
     try {
       let newChannelRes = await this.props.createChannel({
         variables: {
-          ...newChannel
-        }
+          ...newChannel,
+        },
       });
 
       newChannel.id = newChannelRes.data.createChannel.id;
@@ -71,19 +71,19 @@ class CreateChannel extends Component {
       let res = await this.props.addChannelToCircle({
         variables: {
           circle: this.props.activeCircle,
-          channel: newChannel.id
-        }
+          channel: newChannel.id,
+        },
       });
 
       let { name } = res.data.addToCircleOnChannels.circleCircle;
 
       Alert.alert(
-        "Channel Created",
-        `${this.state.name} has been created in ${name}.`
+        'Channel Created',
+        `${this.state.name} has been created in ${name}.`,
       );
       this.props.dispatch(updateChannel(newChannel.id));
 
-      this.props.navigation.navigate("Channel");
+      this.props.navigation.navigate('Channel');
     } catch (err) {
       console.error(new Error(err));
     }
@@ -94,7 +94,7 @@ class CreateChannel extends Component {
     return (
       <Query
         query={GET_CIRCLE_NAME_BY_ID}
-        variables={{ id: this.props.activeCircle || "" }}
+        variables={{ id: this.props.activeCircle || '' }}
       >
         {({ loading, data }) => {
           if (data) {
@@ -103,35 +103,37 @@ class CreateChannel extends Component {
           if (loading || this.state.loading) {
             return (
               <ScreenWrapper styles={[styles.wrapper]}>
-                <UIActivityIndicator color={"#FFFFFF"} />
+                <UIActivityIndicator color={'#FFFFFF'} />
               </ScreenWrapper>
             );
           }
           return (
             <ScreenWrapper styles={[styles.wrapper]}>
               <ScrollView styles={[styles.wrapper]}>
-                <KeyboardAvoidingView behavior="padding">
+                <KeyboardAvoidingView behavior='padding'>
                   <Text style={styles.header}>
                     CREATE A NEW CHANNEL WITHIN {circle.name}.
                   </Text>
                   <Input
-                    placeholder={"Channel Name"}
-                    label={"Channel Name"}
+                    placeholder={'Channel Name'}
+                    label={'Channel Name'}
                     onChangeText={this.updateName}
                     value={name}
+                    placeholderTextColor={'#FFFFFFb7'}
                   />
                   <Input
                     value={description}
                     onChangeText={this.updateDesc}
-                    label={"Description"}
-                    description={"Describe this channel"}
+                    label={'Description'}
+                    description={'Describe this channel'}
                     multiline={true}
+                    placeholderTextColor={'#FFFFFFb7'}
                   />
                   <Text style={styles.disclaimer}>
                     By pressing "Create Channel" you will create a new channel
                     within {circle.name}.
                   </Text>
-                  <PortalButton title="Create Channel" onPress={this.submit} />
+                  <PortalButton title='Create Channel' onPress={this.submit} />
                 </KeyboardAvoidingView>
               </ScrollView>
             </ScreenWrapper>
@@ -144,40 +146,40 @@ class CreateChannel extends Component {
 
 const styles = StyleSheet.create({
   header: {
-    textTransform: "uppercase",
+    textTransform: 'uppercase',
     letterSpacing: 2,
     fontSize: 13,
-    color: "#FFFFFFb7",
-    marginBottom: 25
+    color: '#FFFFFFb7',
+    marginBottom: 25,
   },
   wrapper: {
-    alignItems: "stretch",
-    justifyContent: "flex-start",
-    width: "100%",
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    width: '100%',
     flex: 1,
-    padding: 13
+    padding: 13,
   },
   disclaimer: {
     fontSize: 15,
-    color: "#FFFFFFb7",
-    marginBottom: 20
+    color: '#FFFFFFb7',
+    marginBottom: 20,
   },
   label: {
     fontSize: 18,
     marginBottom: 10,
-    color: "#FFF"
-  }
+    color: '#FFF',
+  },
 });
 
 function mapStateToProps(state) {
   return {
-    user: pull(state, "user"),
-    activeCircle: pull(state, "activeCircle"),
-    circles: pull(state, "circles")
+    user: pull(state, 'user'),
+    activeCircle: pull(state, 'activeCircle'),
+    circles: pull(state, 'circles'),
   };
 }
 
 export default compose(
-  graphql(CREATE_CHANNEL, { name: "createChannel" }),
-  graphql(ADD_CHANNEL_TO_CIRCLE, { name: "addChannelToCircle" })
+  graphql(CREATE_CHANNEL, { name: 'createChannel' }),
+  graphql(ADD_CHANNEL_TO_CIRCLE, { name: 'addChannelToCircle' }),
 )(connect(mapStateToProps)(CreateChannel));
